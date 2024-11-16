15746260db2c81819ab83960af39ede4b2b15df6
Mustafa <mustafabayramoglu29@gmail.com>
2024-11-16 23:12:01 +0300

    adding template pages.

--- blogapp/blogapp/urls.py
+++ blogapp/blogapp/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+#http://127.0.0.1:8000/ => index
+#http://127.0.0.1:8000/index => index
+#http://127.0.0.1:8000/blogs => blogs
+#http://127.0.0.1:8000/blogs/3 => blog details
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
