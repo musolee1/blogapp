e8227ba3f46efb375865eebb5cd0e1b7160a6308
Mustafa <mustafabayramoglu29@gmail.com>
2024-11-23 23:05:01 +0300

    category and blog slugs added.

--- blogapp/blog/admin.py
+++ blogapp/blog/admin.py
@@ -3,9 +3,10 @@ from .models import Blog, Category
 # Register your models here.
 
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ('title', 'is_active', 'is_home')
+    list_display = ('title', 'is_active', 'is_home', 'slug',)
     list_editable = ('is_active', 'is_home')
     search_fields = ('title', 'description')
+    readonly_fields = ('slug',)
     # readonly_fields = ('description',) # This will make the description field read-only
 
 admin.site.register(Blog, BlogAdmin)
--- blogapp/blog/models.py
+++ blogapp/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -8,14 +9,22 @@ class Blog(models.Model):
     description = models.TextField()
     is_active = models.BooleanField(default = False)
     is_home = models.BooleanField(default = False)
-    slug = models.SlugField(null = False)
+    slug = models.SlugField(null=False, blank=True, unique=True, db_index=True, editable = False)
 
     def __str__(self):
         return f"{self.title}"
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
 class Category(models.Model):
     name = models.CharField(max_length=150)
+    slug = models.SlugField(null=False, blank=True, unique=True, db_index=True, editable=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.name}"
+        return self.name
