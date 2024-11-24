c688b6ca89fadd7f78d26c8be10fb769239d5763
Mustafa <mustafabayramoglu29@gmail.com>
2024-11-24 19:09:48 +0300

    urls edited, slug instead of id

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -46,9 +46,9 @@ def blogs(request):
     }
     return render(request, 'blog/blogs.html', context)
 
-def blog_details(request, id):
+def blog_details(request, slug):
     
-    blog = Blog.objects.get(id = id)
+    blog = Blog.objects.get(slug = slug)
 
     return render(request, 'blog/blog-details.html', {
         'blog': blog
